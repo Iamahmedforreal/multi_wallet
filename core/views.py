4794364dc46515c6ed763d3c42fe72af2af3539c
Ahmeddaud11 <shahwah7788@gmail.com>
2025-10-28 02:36:10 +0300

    added change password and logout

--- core/views.py
+++ core/views.py
@@ -6,8 +6,14 @@ from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken, AccessToken
 from rest_framework import permissions
 from .models import AuditLog
-
+from rest_framework.permissions import IsAuthenticated
+from django.utils import timezone
 import logging
+logger = logging.getLogger('auth')
+from django.db import transaction
+
+
+
 
 class registerView(APIView):
     permission_classes = [permissions.AllowAny]
@@ -84,7 +90,59 @@ class loginView(APIView):
         else:
             ip = request.META.get('REMOTE_ADDR')
         return ip
+    """ Logout View """
+class logoutView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        """ Log logout action if logout is successful"""
+        try:
+            AuditLog.objects.create(
+                user=request.user,
+                action='Logout',
+                ip_address=request.META.get('REMOTE_ADDR'),
+                user_agent=request.META.get('HTTP_USER_AGENT', ''),
+            )
+            return Response({"message": "Logout successful"}, status=200)
+        except Exception as e:
+            logger.exception("Failed to write AuditLog for logout")
+            return Response({"error": str(e)}, status=400)
         
+    """method to get client ip address"""
+    @staticmethod
+    def get_client_ip(request):
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0]
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+        return ip
     
+class changePasswordView(APIView):
+    permission_classes = [IsAuthenticated]
+    """ View to change user password """
+    def post(self , request):
+        serializer  = changePasswordSerializer(data=request.data , context={'request':request})
+        if not serializer.is_valid():
+            return Response(serializer.errors ,status=400)
+        
+        user = request.user
+        new_password = serializer.validated_data['new_password']
+        
+        """ Change the user's password and log the action """
+        try:
+            with transaction.atomic():
+                user.set_password(serializer.validated_data['new_password'])
+                user.save()
+                AuditLog.objects.create(
+                    user=user,
+                    action='Password Change',
+                    ip_address=request.META.get('REMOTE_ADDR'),
+                    user_agent=request.META.get('HTTP_USER_AGENT', ''),
+                )
+        except Exception as e:
+            logger.exception("Failed to write AuditLog for password change")
+            return Response({"error": str(e)}, status=400)
+        return Response({"message":"Password changed successfully"}, status=200)
 
-
+       
